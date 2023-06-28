7020c55bf319478afcc0ff5d8687198f379aeb51
neeti-nk47 <neeti.nk47@gmail.com>
2023-06-28 17:01:03 +0530

    dynamic page added

---
+++ pages/[meetupID].js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetail
+      title="1st"
+      image="https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+      address="Paris"
+    />
+  );
+}
+
+export default MeetupDetailsPage;
